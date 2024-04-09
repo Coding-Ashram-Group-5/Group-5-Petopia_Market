2e840d7971895583c66df39007600ec0022d9774
Ariaj Sarkar <rajsarkar02205@gmail.com>
2024-04-09 19:47:56 +0530

    fix: error jwtDecode

--- frontend/src/components/pages/securepage/SecuredRoute.tsx
+++ frontend/src/components/pages/securepage/SecuredRoute.tsx
@@ -1,17 +1,38 @@
 import { ReactNode } from 'react';
 import { Navigate } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode, JwtPayload } from "jwt-decode";
 import api from "../../utils/api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../../utils/constants";
 import { useState, useEffect } from "react";
 
-
 function SecuredRoute({ children }: { children: ReactNode }) {
-    const [isAuthorized, setIsAuthorized] = useState(null);
+    const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null);
 
     useEffect(() => {
-        auth().catch(() => setIsAuthorized(false))
-    }, [])
+        const auth = async () => {
+            const token = localStorage.getItem(ACCESS_TOKEN);
+            if (!token) {
+                setIsAuthorized(false);
+                return;
+            }
+            const decoded: JwtPayload | undefined = jwtDecode(token); // Define decoded as JwtPayload or undefined
+            if (!decoded || typeof decoded.exp !== 'number') { // Check if decoded or decoded.exp is undefined
+                setIsAuthorized(false);
+                return;
+            }
+            const tokenExpiration = decoded.exp;
+            const now = Date.now() / 1000;
+
+            if (tokenExpiration < now) {
+                await refreshToken();
+            } else {
+                setIsAuthorized(true);
+            }
+        };
+
+        auth().catch(() => setIsAuthorized(false));
+
+    }, []);
 
     const refreshToken = async () => {
         const refreshToken = localStorage.getItem(REFRESH_TOKEN);
@@ -20,31 +41,14 @@ function SecuredRoute({ children }: { children: ReactNode }) {
                 refresh: refreshToken,
             });
             if (res.status === 200) {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access)
-                setIsAuthorized(true)
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                setIsAuthorized(true);
             } else {
-                setIsAuthorized(false)
+                setIsAuthorized(false);
             }
         } catch (error) {
             console.log(error);
             setIsAuthorized(false);
-        }
-    };
-
-    const auth = async () => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (!token) {
-            setIsAuthorized(false);
-            return;
-        }
-        const decoded = jwtDecode(token);
-        const tokenExpiration = decoded.exp;
-        const now = Date.now() / 1000;
-
-        if (tokenExpiration < now) {
-            await refreshToken();
-        } else {
-            setIsAuthorized(true);
         }
     };
 
