83f67b9a1fcf3b3eec15b8f6d8cd20d850dd60fa
narendra <narendranishad59@gmail.com>
2024-05-23 12:03:26 +0530

    Add Api Method

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,11 +6,15 @@ import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import Login from "./components/Auth/pages/Login";
 import Register from "./components/Auth/pages/Register";
-import Home from "./components/Ui/Home.tsx";
+import Home from "./Pages/Home.tsx";
 import NotFound from "./components/NotFound.tsx";
-import Products from "./components/Ui/Pages/ProductPage/Products.tsx";
-import Pets from "./components/Ui/Pets/Pets.tsx";
-import PetDetails from "./components/Ui/Pets/PetDetails.tsx";
+import Products from "@/Pages/ProductPage/Products.tsx";
+import Pets from "@/Pages/Pets/Pets.tsx";
+import PetDetails from "@/Pages/Pets/PetDetails.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import ProductDetails from "./Pages/ProductPage/ProductDetails.tsx";
+
+const queryClient = new QueryClient()
 
 function RegisterAndLogout() {
     return <Register />;
@@ -26,6 +30,7 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
             <BrowserRouter>
                 <Routes>
@@ -42,10 +47,15 @@ root.render(
                             path="/pets/getDetails/:id"
                             element={<PetDetails />}
                         />
+                         <Route
+                            path="/products/getDetails/:id"
+                            element={<ProductDetails />}
+                        />
                         <Route path="*" element={<NotFound />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
         </ThemeProvider>
+        </QueryClientProvider>
     </React.StrictMode>,
 );
