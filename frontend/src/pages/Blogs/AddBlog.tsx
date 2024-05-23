57cbb88db6e79eb33726adf7d25caae8da747ac5
Tarak(taraksh01) <135436617+taraksh01@users.noreply.github.com>
2024-05-23 11:25:01 +0530

    Create blog related pages

---
+++ frontend/src/pages/Blogs/AddBlog.tsx
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { postBlog } from "@/lib/api";
+import { useNavigate } from "react-router-dom";
+import { X, Image } from "lucide-react";
+
+function BlogPostForm() {
+    const navigate = useNavigate();
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [content, setContent] = useState("");
+    const [cover, setCover] = useState(null);
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (!title || !category || !content || !cover) {
+            setError("All fields are required.");
+            return;
+        }
+        setError("");
+
+        const data = await postBlog({ title, category, content, image: cover });
+
+        if (data.success) {
+            navigate(`/blogs/${data.data._id}`);
+        }
+    };
+
+    const handleCoverChange = (event) => {
+        setCover(event.target.files[0]);
+    };
+
+    const removeCoverImage = () => {
+        setCover(null);
+    };
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+            className="p-6 space-y-4 shadow-md rounded-md"
+        >
+            {error && <p className="text-red-500">{error}</p>}
+            <div className="flex items-center">
+                <label
+                    htmlFor="cover-input"
+                    className="text-lg text-gray-700 dark:text-gray-200 bg-transparent cursor-pointer"
+                >
+                    {cover ? (
+                        <img
+                            src={cover && URL.createObjectURL(cover).toString()}
+                            alt="Image Preview"
+                            className="w-80 h-48 m-2 rounded flex items-center justify-evenly"
+                        />
+                    ) : (
+                        <div className="flex items-center gap-2">
+                            <Image />
+                            Add Cover
+                        </div>
+                    )}
+                </label>
+                <input
+                    id="cover-input"
+                    type="file"
+                    accept="image/*"
+                    value={cover && null}
+                    onChange={handleCoverChange}
+                    style={{ display: "none" }}
+                />
+                {cover && (
+                    <button
+                        type="button"
+                        onClick={removeCoverImage}
+                        disabled={!cover}
+                        className={`font-bold text-gray-700 dark:text-gray-200 bg-transparent rounded hover:text-red-700`}
+                    >
+                        <X />
+                    </button>
+                )}
+            </div>
+            <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Blog Title...."
+                className="mt-1 block w-full text-3xl outline-none rounded-md text-gray-700 dark:text-gray-200 bg-transparent shadow-sm py-1"
+            />
+            <input
+                type="text"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                placeholder="Category 1, Category 2,....."
+                className="mt-1 block w-full text-2xl outline-none rounded-md text-gray-700 dark:text-gray-200 bg-transparent shadow-sm py-1"
+            />
+            <div className="h-[500px]">
+                <ReactQuill
+                    theme="snow"
+                    value={content}
+                    onChange={setContent}
+                    className="h-5/6 bg-transparent w-full text-xl rounded-md my-2 lg:my-1"
+                />
+            </div>
+            <div className="flex justify-center items-center">
+                <input
+                    type="submit"
+                    value="Submit"
+                    className="px-4 py-2 font-bold text-xl cursor-pointer text-white bg-pink-500  hover:bg-pink-700 rounded-md -mt-810"
+                />
+            </div>
+        </form>
+    );
+}
+
+export default BlogPostForm;
---
+++ frontend/src/pages/Blogs/Comment.tsx
@@ -0,0 +1,88 @@
+import usePersonStore from "@/lib/Utils/zustandStore";
+import { Ellipsis, User } from "lucide-react";
+import { useState } from "react";
+
+const Comment = ({
+    comment,
+    setUpdating,
+    editComment,
+    delComment,
+}: {
+    comment: {
+        _id: string;
+        ownerFirstName: string;
+        comment: string;
+        owner: string;
+    };
+    setUpdating: any;
+    editComment: any;
+    delComment: any;
+}) => {
+    const loggedInUser = usePersonStore((state) => state._id);
+    const [showBtn, setShowBtn] = useState(false);
+    const [showMore, setShowMore] = useState(false);
+    console.log(comment);
+
+    return (
+        <div className="flex flex-col gap-2 px-4 py-2 bg-gray-200 dark:bg-[#071b4d]  m-2 rounded-lg font-leag text-lg leading-6">
+            <div className="font-semibold text-xl flex items-center justify-between relative">
+                <div className="flex gap-2 items-center">
+                    <User
+                        size={28}
+                        strokeWidth={3}
+                        className="rounded-full border-2 border-gray-800 dark:border-white pt-1"
+                    />
+                    {comment.ownerFirstName}
+                </div>
+                {loggedInUser === comment.owner && (
+                    <Ellipsis
+                        className="cursor-pointer"
+                        onClick={() => {
+                            setShowBtn(true);
+                        }}
+                    />
+                )}
+                {showBtn && (
+                    <div className="absolute right-2 top-4 p-2 rounded-lg text-gray-800 font-medium bg-gray-300 flex flex-col justify-between items-start">
+                        <button
+                            onClick={() => {
+                                editComment(comment.comment);
+                                setUpdating(comment._id);
+                                setShowBtn(false);
+                            }}
+                        >
+                            Edit
+                        </button>
+                        <button
+                            onClick={() => {
+                                delComment(comment._id);
+                                setShowBtn(false);
+                            }}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                )}
+            </div>
+            <div className="flex-nowrap text-justify py-1">
+                {comment.comment.length > 100 ? (
+                    <span>
+                        {showMore
+                            ? comment.comment.slice(1, 100) + "..."
+                            : comment.comment}
+                        <span
+                            onClick={() => setShowMore(!showMore)}
+                            className="text-pink-600 cursor-pointer"
+                        >
+                           {"  "} show {showMore ? "more" : "less"}
+                        </span>
+                    </span>
+                ) : (
+                    <span>{comment.comment}</span>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Comment;
