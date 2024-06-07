823f182e87de9fc22d876da4f588fae748fc0f55
JavidSumra <javidsumra135@gmail.com>
2024-06-07 16:42:19 +0530

    Adding cronJob for Zero Down Timecls

---
+++ Backend/src/cronJob/index.ts
@@ -0,0 +1,13 @@
+const API_ENDPOINT: string = process.argv.slice(2)[1]; //!ex --URL http://localhost:5173
+
+if (!API_ENDPOINT && API_ENDPOINT.startsWith('http')) {
+  throw new Error('Please Provide Valid Arguments');
+}
+
+const sendRequestToServer = (): void => {
+  fetch(`${API_ENDPOINT}/api/v1/health`)
+    .then(() => console.log('Server is Running'))
+    .catch((err) => console.log(err));
+};
+
+sendRequestToServer();
