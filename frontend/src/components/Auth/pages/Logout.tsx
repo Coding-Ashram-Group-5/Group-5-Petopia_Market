7d10c118e5a08c1aeffc128923274d4ce83d228a
Ariaj Sarkar <rajsarkar02205@gmail.com>
2024-06-28 22:29:45 +0530

    fix: relogin

--- frontend/src/components/Auth/pages/Logout.tsx
+++ frontend/src/components/Auth/pages/Logout.tsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { logout } from '../../../lib/api';
+import { logout } from '@/lib/api';
+import usePersonStore from '@/lib/Utils/zustandStore'; // Adjust the path according to your project structure
 
 interface LogoutProps {
-  buttonLabel: string;
+    buttonLabel: string;
 }
 
 const Logout: React.FC<LogoutProps> = ({ buttonLabel }) => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const updatePerson = usePersonStore(state => state.updatePerson);
+    const setLoggedIn = usePersonStore(state => state.setLoggedIn);
 
-  const handleLogout = async () => {
-    const confirmed = window.confirm('Are you sure you want to logout?');
-    if (confirmed) {
-      try {
-        await logout();
-        navigate('/'); // Redirect to register page
-        // Auto-refresh after logout
-        window.location.reload();
-      } catch (error) {
-        console.error(error); // Handle error
-      }
-    }
-  };
+    const handleLogout = async () => {
+        const confirmed = window.confirm('Are you sure you want to logout?');
+        if (confirmed) {
+            try {
+                await logout();
+                updatePerson('', '', '', '', { publicId: '', url: '' });
+                setLoggedIn(false); // Update loggedIn state
+                navigate('/');
+            } catch (error) {
+                console.error(error); // Handle error
+            }
+        }
+    };
 
-  return (
-    <button type="button" onClick={handleLogout}>{buttonLabel}</button>
-  );
+    return (
+        <button type="button" onClick={handleLogout}>{buttonLabel}</button>
+    );
 };
 
 export default Logout;
