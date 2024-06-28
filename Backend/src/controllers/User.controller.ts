c82eeebdb0ade63b0bfb919322df78046db6fecc
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2024-06-28 18:54:08 +0530

    Update User.controller.ts

--- Backend/src/controllers/User.controller.ts
+++ Backend/src/controllers/User.controller.ts
@@ -178,7 +178,6 @@ const logoutUser = AsyncHandler(async (req: IGetUserAuthInfoRequest, res: Respon
     res
       .status(200)
       .clearCookie('authToken')
-      .clearCookie('refreshToken')
       .json(new APIResponse('User Logged Out Successfully', 200, []));
   } catch (error) {
     console.log(error);
